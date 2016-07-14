2bd75e89f2a79e0668c0fdf76769d5e9acb56a6d
XuXiang <xux@geohey.com>
2016-07-14 20:22:06 +0800

    default caffe model set to bvlc, it's much faster than resent

--- config.py
+++ config.py
@@ -88,7 +88,7 @@ caffe_root = '../caffe/'
 caffe_extract_features_bin = '../caffe/build/tools/extract_features.bin'
 
 # caffe model, can be resnet or bvlc
-caffemodel = 'resnet'
+caffemodel = 'bvlc'
 
 if caffemodel == 'resnet':
     ## deep-residual-networks
