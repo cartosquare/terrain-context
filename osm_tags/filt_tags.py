d5ec41bc6687c048969d9e28a1bbba5b900f4290
XuXiang <xux@geohey.com>
2016-07-14 15:13:14 +0800

    label添加数字映射

--- osm_tags/filt_tags.py
+++ osm_tags/filt_tags.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 from config import all_tags_csv, tags_csv, unique_tags_csv
 
@@ -8,6 +9,7 @@ f3 = open(unique_tags_csv, 'w')
 
 lines = f1.readlines()
 unique_labels = set()
+labels_map = {}
 first_line = True
 for line in lines:
     if first_line:
@@ -16,15 +18,24 @@ for line in lines:
     else:
         (tag, value, osm_type, label, label_cn, only_bj, use) = line.strip().split(',')
         if int(use):
-            f2.write('%s,%s,%s,%s,%s,%s,%s\n' % (tag, value, osm_type, tag + '-' + value, label_cn, only_bj, use))
-            unique_labels.add(tag + '-' + value)
+            new_label = tag + '-' + value
+            f2.write('%s,%s,%s,%s,%s,%s,%s\n' % (tag, value, osm_type, new_label, label_cn, only_bj, use))
+            unique_labels.add(new_label)
+            if new_label not in labels_map:
+                labels_map[new_label] = label_cn
+
 f1.close()
 f2.close()
 
+print len(unique_labels)
+print len(labels_map)
+
 labels_list = sorted(unique_labels)
+count = 0
 for item in labels_list:
-    f3.write(item)
+    f3.write('%s,%s,%d' % (item, labels_map[item], count))
     f3.write('\n')
+    count += 1
 f3.close()
 
 print '#labels = %d' % (len(labels_list))
