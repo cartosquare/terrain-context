7e266f43ece4637e8a012df7636387dc66611119
XuXiang <xux@geohey.com>
2016-07-21 21:25:55 +0800

    switch x and y when writing key files

--- features/write_keys.py
+++ features/write_keys.py
@@ -1,15 +1,23 @@
 import sys
 import os
+import shutil
+
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 from config import L18_image_list, L18_keys_file
 import leveldb
 
+# remove if exists
+if os.path.exists(L18_keys_file):
+    shutil.rmtree(L18_keys_file)
 
 db = leveldb.LevelDB(L18_keys_file)
 with open(L18_image_list, 'r') as f:
     count = 0
     for line in f:
         file_path, x, y = line.strip().split()
-        filename = x + '_' + y
+        # Warning: in the L18 image list file, the position of x and y are switched.
+        filename = y + '_' + x
         db.Put(filename, format(count, '010d'))
         count += 1
+        if count % 10000 == 0:
+            print 'processed %d' % (count)
