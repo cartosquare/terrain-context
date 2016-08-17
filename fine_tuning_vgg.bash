7a72360a612c64ee43e9449707f337915137deac
XuXiang <xux@geohey.com>
2016-08-17 15:16:47 +0800

    修复文件名错误

--- fine_tuning_vgg.bash
+++ fine_tuning_vgg.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../caffe/build/tools/caffe train -solver vgg_tuning/solver.prototxt -weights ./vgg_tuning/VGG_ILSVRC_16_layers.caffemodel.caffemodel -gpu 0
+../caffe/build/tools/caffe train -solver vgg_tuning/solver.prototxt -weights ./vgg_tuning/VGG_ILSVRC_16_layers.caffemodel -gpu 0
